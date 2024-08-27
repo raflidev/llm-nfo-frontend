2f2a6cdeccf85d15e3883a80857332c8020d8170
Rafli Ramadhan <rafliramdhn@gmail.com>
2024-08-27 14:34:04 +0700

    fix login issue

--- src/components/organisms/GridTopic.jsx
+++ src/components/organisms/GridTopic.jsx
@@ -92,12 +92,8 @@ function GridTopic(props) {
         client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID
       }
       mutateLogin(data)
+      window.location.reload()
     },
-    onError: (error) => {
-      // toast.error(error.message, {
-      //   transition: Slide
-      // })
-    }
   });
 
 
